0bf94d600fb640369ffff0f4e328aebbc54da2c0
luyvannda <philips.joe329@gmail.com>
2023-10-17 15:25:42 +0700

    add rings and circle pattern to ProfilePhoto component

--- src/components/ProfilePhoto.jsx
+++ src/components/ProfilePhoto.jsx
@@ -1,8 +1,14 @@
 
 export default function ProfilePhoto() {
   return (
-    <div className="profile_photo">
-      <img src="./images/image-profile.png" alt="my profile photo"></img>
+    <div className="profile-container">
+      <div className="profile_photo">
+        <img className="photo" src="./images/image-profile.png" alt="my profile photo"></img>
+      </div>
+
+      <img className="rings" src="./images/pattern-rings.svg" alt="rings pattern"></img>
+
+      <img className="circle" src="./images/pattern-circle.svg" alt="circle pattern"></img>
     </div>
   )
 }
