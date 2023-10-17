242490f120aca08813311bec79b770bcca479083
luyvannda <philips.joe329@gmail.com>
2023-10-17 16:06:55 +0700

    add hero text and green underline in profile name

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.scss'
+import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import ProfilePhoto from './components/ProfilePhoto'
 
@@ -9,8 +10,9 @@ function App() {
       <header className='header'>
         <Navbar />
       </header>
-      <section className='hero'>
+      <section className='hero-container'>
         <ProfilePhoto />
+        <Hero />
       </section>
     </div>
   )
---
+++ src/components/Hero.jsx
@@ -0,0 +1,7 @@
+export default function Hero() {
+  return (
+    <div className="hero">
+      <h2>Nice to meet you! I&apos;m <span>Luy Vannda.</span></h2>
+    </div>
+  )
+}
