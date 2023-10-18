75d869eecb57d21c7833d16a23cde0d98e8f8a1f
luyvannda <philips.joe329@gmail.com>
2023-10-18 10:55:14 +0700

    add styles to .hero-text

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,8 @@
 export default function Hero() {
   return (
     <div className="hero">
-      <h2>Nice to meet you! I&apos;m <span>Luy Vannda.</span></h2>
+      <h2 className="hero-title">Nice to meet you! I&apos;m <span>Luy Vannda.</span></h2>
+      <p className="hero-text">Based in Cambodia, I&apos;m a front-end developer passionate about building accessible web apps that users love.</p>
     </div>
   )
 }
