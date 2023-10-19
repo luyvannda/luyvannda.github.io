bc75e03e4780b0e5fc9576254f7224d0eb6b491c
luyvannda <philips.joe329@gmail.com>
2023-10-19 11:10:49 +0700

    add projects title and contact btn to Projects component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.scss'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import ProfilePhoto from './components/ProfilePhoto'
+import Projects from './components/Projects'
 import Skills from './components/Skills'
 
 function App() {
@@ -15,11 +16,8 @@ function App() {
         <ProfilePhoto />
         <Hero />
       </section>
-
-      <section>
-        <Skills />
-      </section>
-
+      <Skills />
+      <Projects />
     </div>
   )
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,16 @@
+import ContactBtn from "./ContactBtn";
+
+export default function Projects() {
+
+  return (
+    <div className="projects">
+
+      <div className="projects-heading">
+        <h2>Projects</h2>
+        <ContactBtn />
+      </div>
+
+
+    </div>
+  )
+}
