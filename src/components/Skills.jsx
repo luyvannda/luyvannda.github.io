57b4373294c868b8024c140966205261a56a891a
luyvannda <philips.joe329@gmail.com>
2023-10-19 10:10:56 +0700

    pass skillsData object as props to create reusable component for Skills

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,8 +1,16 @@
-export default function Skills() {
-  return (
-    <div className="skills">
-      <h1>HTML</h1>
-      <p>1 Year Experience</p>
-    </div>
-  )
+import { skillsData } from "./skillsData"
+
+function Skills() {
+
+  const skillsElement = skillsData.map((skill) => {
+    return (
+      <div key={skill.id} className="skills">
+        <h1>{skill.name}</h1>
+        <p>{skill.experience} Experience</p>
+      </div>
+    )
+  })
+  return <div>{skillsElement}</div>
 }
+
+export default Skills
---
+++ src/components/skillsData.jsx
@@ -0,0 +1,38 @@
+export const skillsData = [
+
+  {
+    id: 1,
+    name: "HTML",
+    experience: "1 Year"
+  },
+
+  {
+    id: 2,
+    name: "CSS",
+    experience: "1 Year"
+  },
+
+  {
+    id: 3,
+    name: "JavaScript",
+    experience: "1 Year"
+  },
+
+  {
+    id: 4,
+    name: "Accessibility",
+    experience: "1 Year"
+  },
+
+  {
+    id: 5,
+    name: "React",
+    experience: "6 Months"
+  },
+
+  {
+    id: 6,
+    name: "SASS",
+    experience: "6 Months"
+  }
+]
