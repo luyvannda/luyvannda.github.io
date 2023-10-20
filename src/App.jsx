ecb3c452a4aab999cc5204b74a1ae97b57888269
luyvannda <philips.joe329@gmail.com>
2023-10-20 16:03:05 +0700

    experiment with google form to collect respondent info.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.scss'
+import ContactForm from './components/ContactForm'
 import Hero from './components/Hero'
 import Navbar from './components/Navbar'
 import ProfilePhoto from './components/ProfilePhoto'
@@ -12,12 +13,21 @@ function App() {
       <header className='header'>
         <Navbar />
       </header>
-      <section className='hero-container'>
-        <ProfilePhoto />
-        <Hero />
-      </section>
-      <Skills />
-      <Projects />
+      <main>
+        <section className='hero-container'>
+          <ProfilePhoto />
+          <Hero />
+        </section>
+
+        <section>
+          <Skills />
+          <Projects />
+        </section>
+
+        <section>
+          <ContactForm />
+        </section>
+      </main>
     </div>
   )
 }
---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,8 @@
+export default function ContactForm() {
+  return (
+    <form>
+      <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdbiByqzfTtmBHAYF0tll4_XwcrHdoPWR8GEsi1pjiScK3Nlw/viewform?embedded=true" width="640" height="849" frameBorder="0" marginHeight="0" marginWidth="0">Loading…</iframe>
+
+    </form>
+  )
+}
