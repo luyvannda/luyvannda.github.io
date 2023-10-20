498b224246c0dfd2c7d75e488fd0a44a4b221f68
luyvannda <philips.joe329@gmail.com>
2023-10-20 10:53:08 +0700

    add 3 more data to Project component

--- src/components/projectsData.jsx
+++ src/components/projectsData.jsx
@@ -6,5 +6,32 @@ export const projectsData = [
     skills: "HTML CSS",
     livePreview: "https://luyvannda.github.io/personal-portfolio-webpage/",
     sourceCode: "https://github.com/luyvannda/personal-portfolio-webpage"
+  },
+
+  {
+    id: 2,
+    imgCover: "odin-landing-page.webp",
+    title: "LANDING PAGE",
+    skills: "HTML CSS",
+    livePreview: "https://luyvannda.github.io/odin-landing-page/",
+    sourceCode: "https://github.com/luyvannda/odin-landing-page"
+  },
+
+  {
+    id: 3,
+    imgCover: "survey-form.webp",
+    title: "SURVEY FORM",
+    skills: "HTML CSS",
+    livePreview: "https://luyvannda.github.io/survey-form/",
+    sourceCode: "https://github.com/luyvannda/survey-form"
+  },
+
+  {
+    id: 4,
+    imgCover: "odin-calculator.webp",
+    title: "CALCULATOR",
+    skills: "HTML CSS JAVASCRIPT",
+    livePreview: "https://luyvannda.github.io/odin-calculator/",
+    sourceCode: "https://github.com/luyvannda/odin-calculator"
   }
 ]
