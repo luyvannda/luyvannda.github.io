604a2801a85195d6de2a8c4eb47aefdcb228a776
luyvannda <philips.joe329@gmail.com>
2023-10-25 15:34:33 +0700

    add media queries for desktop device

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -3,8 +3,11 @@ export default function ContactForm() {
 
     <div className="form-container">
       <div className="contact-form">
-        <h2>Contact</h2>
-        <p>I would love to hear about your project and how I could help. Please fill in the form, and I&apos;ll get back to you as soon as possible.</p>
+
+        <div className="form-heading">
+          <h2>Contact</h2>
+          <p>I would love to hear about your project and how I could help. Please fill in the form, and I&apos;ll get back to you as soon as possible.</p>
+        </div>
 
         <form
           action="https://getform.io/f/e84d47c6-f990-424a-b7fe-56b37f894479"
