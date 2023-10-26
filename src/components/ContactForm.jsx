017b1d28ee8d47b8db558b1bf06c25f669da7a96
luyvannda <philips.joe329@gmail.com>
2023-10-26 16:13:18 +0700

    fix the cascade error in Contact Form and set responsive font size for components heading with clamp

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,9 +1,9 @@
 export default function ContactForm() {
   return (
 
-    <div className="form-container">
-      <div className="contact-form">
+    <div className="form-background">
 
+      <div className="contact-form">
         <div className="form-heading">
           <h2>Contact</h2>
           <p>I would love to hear about your project and how I could help. Please fill in the form, and I&apos;ll get back to you as soon as possible.</p>
@@ -29,9 +29,11 @@ export default function ContactForm() {
         </form>
       </div>
 
+
       <img className="form-rings" src="images/pattern-rings.svg" alt="rings pattern decoration"></img>
 
       <div className="bottom-border"></div>
+
     </div>
   )
 }
