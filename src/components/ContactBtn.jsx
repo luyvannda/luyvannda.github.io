0b9a6efe7e29229df55b465e4f7b7d5f3881a0e6
luyvannda <philips.joe329@gmail.com>
2023-10-28 10:25:36 +0700

    embed image in One Drive and use it in preview thumbnail link

--- src/components/ContactBtn.jsx
+++ src/components/ContactBtn.jsx
@@ -1,7 +1,7 @@
 export default function ContactBtn() {
   return (
     <div className="contact-btn">
-      <a className="contact-me" href="#">CONTACT ME</a>
+      <a className="contact-me" href="mailto:luyvannda@gmail.com">CONTACT ME</a>
     </div>
   )
 }
--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -9,7 +9,7 @@ export default function ContactForm() {
           <p>I would love to hear about your project and how I could help. Please fill in the form, and I&apos;ll get back to you as soon as possible.</p>
         </div>
 
-        <form
+        <form id="form-id"
           action="https://getform.io/f/e84d47c6-f990-424a-b7fe-56b37f894479"
           method="POST">
 
